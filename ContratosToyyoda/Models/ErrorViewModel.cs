fbdcd650739693b07e10a537ecd41254400e6c7a
harry.algoritmico1@gmail.com <harry.algoritmico1@gmail.com>
2023-05-03 19:36:51 -0600

    Add project files.

---
+++ ContratosToyyoda/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContratosToyyoda.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
