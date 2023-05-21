691fc19a5e99c34920f82f397c8627df9d655e00
harry.algoritmico1@gmail.com <harry.algoritmico1@gmail.com>
2023-05-20 18:06:54 -0600

    ha este punto ya puedo subir plantillas, agregarle los datos de un modelo y mandarlo en pdf, ahora intentare agrear un campo a el modelo contrato, necesito un campo con el correo.

--- ContratosToyyoda/Controllers/ContratosController.cs
+++ ContratosToyyoda/Controllers/ContratosController.cs
@@ -3,6 +3,8 @@ using ContratosToyyoda.Data;
 using ContratosToyyoda.Data.Services;
 using ContratosToyyoda.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -12,16 +14,30 @@ using Microsoft.Win32;
 using System;
 using System.Diagnostics.Contracts;
 using System.Globalization;
+using Microsoft.AspNetCore.Hosting;
+using ContratosToyyoda.Data.ViewModels;
+using System.IO;
+using System.Threading.Tasks;
+using Xceed.Words.NET;
+using System.Xml.Linq;
+using System.IO;
+using Aspose.Words;
+using Aspose.Words.Saving;
+
 
 namespace ContratosToyyoda.Controllers
 {
+    
     public class ContratosController : Controller
     {
         private readonly IContratosService _service;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ContratosController(IContratosService service)
+        public ContratosController(IContratosService service, IWebHostEnvironment webHostEnvironment)
         {
             _service = service;
+            _webHostEnvironment = webHostEnvironment;
+
         }
         public async Task<IActionResult> Index()
         {
@@ -159,37 +175,31 @@ namespace ContratosToyyoda.Controllers
                         if (values.Length >= 5) // Asegurar que haya suficientes campos en el registro
                         {
 
-                            string nombre = values[0];
-
-                           
-                            string apellido = values[1];
-                           
+                            string nombre = values[0];                                                      
+                            string apellido = values[1];                           
                             string sueldoStr = values[2];
-                            string tipoContratoStr = values[3];
-                            Console.WriteLine(" Convirtio sueldo en  " + sueldoStr);
+                            string tipoContratoStr = values[3]; 
                             string fechaIngresoStr = values[4];
                             string fechaEmisionStr = values[5];
-
                             string idPaisStr = values[6];
                             string idUserStr = values[7];
 
                             if (Enum.TryParse(tipoContratoStr, out TipoContrato tipoContrato)) {
-                                Console.WriteLine(" Convirtio tipo contrato ");
-                                
+                              
                                 if (double.TryParse(sueldoStr, out double sueldo)) {
-                                    Console.WriteLine(" Convirtio Sueldo ");
+                                 
 
                                     if (int.TryParse(idPaisStr, out int idPais)) {
-                                        Console.WriteLine(" Convirtio Pais ");
+                                  
                                         if (int.TryParse(idPaisStr, out int idUser)) {
-                                           Console.WriteLine(" Convirtio ID user  ");
+                                         
                                             if (DateTime.TryParseExact(fechaEmisionStr, "dd/MM/yy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime fechaEmision)) {
-                                                Console.WriteLine(" Convirtio  fechaEmisionStr  ");
+                                               
                                                 if ( DateTime.TryParseExact(fechaIngresoStr, "dd/MM/yy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime fechaIngreso))
 
                                                    {
                                                     // Crear un nuevo objeto y agregarlo a la lista
-                                                    Console.WriteLine(" Convirtio  fechaIngresoStr  ");
+                                                 
                                                     var nuevoContrato = new NuevoContratoVM
                                                     {
                                                         nombre = nombre,
@@ -230,14 +240,6 @@ namespace ContratosToyyoda.Controllers
 
 
 
-                    foreach (var contrato in contratos)
-                    {
-                        Console.WriteLine($"Nombre: {contrato.nombre}");
-                        Console.WriteLine($"Fecha: {contrato.fechaEmision}");
-                        Console.WriteLine($"Sueldo: {contrato.sueldo}");
-                        Console.WriteLine($"IdPaisOrigen: {contrato.idPais}");
-                        Console.WriteLine();
-                    }
 
                     
                 }
@@ -246,6 +248,121 @@ namespace ContratosToyyoda.Controllers
             }
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Plantilla()
+        {
+
+
+            return View();
+        }
+
+        
+
+        [HttpPost]
+        public async Task<IActionResult> Plantilla(IFormFile fileInput)
+        {
+            string filePath;
+
+            if (fileInput != null && fileInput.Length > 0)
+                {
+                Console.WriteLine(_webHostEnvironment.WebRootPath);
+                Console.WriteLine(fileInput.FileName);
+                filePath = Path.Combine(_webHostEnvironment.WebRootPath + "/Plantilla/",fileInput.FileName);
+                Console.WriteLine("Concatela la cadena *********");
+                Console.WriteLine(filePath);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        fileInput.CopyTo(stream);
+                    }
+                }
+            
+            else
+            {
+
+            }
+
+            filePath = Path.Combine(_webHostEnvironment.WebRootPath + "/Plantilla/", fileInput.FileName);
+
+            // Pasar la ruta del archivo a la vista mediante el modelo
+            var model = new PlantillaViewModel(filePath);
+            string fileContent = System.IO.File.ReadAllText(filePath);
+            ViewBag.FileContent = fileContent;
+
+            return View(model);
+        }
+
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Imprimir(int id)
+        {
+           // Obtener los datos del contrato desde la base de datos
+            var contratodetalles = await _service.GetContratoByIdAsync(id);
+            string filePath = "C:/Development/ContratosToyyoda/ContratosToyyoda/wwwroot/Plantilla/CONTRATO DE EMPLEO PLANTILLA.docx";
+            // Ruta de la plantilla de Word
+
+            // Cargar la plantilla de Word
+            DocX doc;
+            doc = DocX.Load(filePath);
+            Console.WriteLine(contratodetalles.nombre);
+            // Reemplazar los marcadores de posición en la plantilla con los datos del contrato
+            doc.ReplaceText("[nombre]", contratodetalles.nombre);
+            doc.ReplaceText("[apellido]", contratodetalles.apellido);
+            doc.ReplaceText("[tipoContrato]", contratodetalles.tipoContrato.ToString());
+            doc.ReplaceText("[sueldo]", contratodetalles.sueldo.ToString());
+            doc.ReplaceText("[idPais]", contratodetalles.idPais.ToString());
+            doc.ReplaceText("[idUser]", contratodetalles.idUser.ToString());
+            doc.ReplaceText("[fechaEmision]", contratodetalles.fechaEmision.ToString("dd/MM/yyyy"));
+            doc.ReplaceText("[fechaIngreso]", contratodetalles.fechaIngreso.ToString("dd/MM/yyyy"));
+            // Guardar el documento modificado en una ubicación temporal
+            string rutaTemporal = Path.Combine(Path.GetTempPath(), "contrato_temp.docx");
+            doc.SaveAs(rutaTemporal);
+            string idstr = contratodetalles.Id.ToString();
+            // Generar el archivo PDF a partir del documento Word modificado
+            string rutaPdf = Path.Combine("C:/Development/ContratosToyyoda/ContratosToyyoda/wwwroot/Contratos/", idstr+".pdf");
+            Console.WriteLine(rutaPdf);
+
+            // Load the word file to be converted to PDF
+            Document sampleDocx = new Document(rutaTemporal);
+
+            // Instantiate the PdfSaveOptions class object before converting the Docx to PDF
+            PdfSaveOptions options = new PdfSaveOptions();
+
+            // Set page mode to full screen while opening it in a viewer
+            options.PageMode = PdfPageMode.FullScreen;
+
+            // Set the output PDF document compliance mode
+            options.Compliance = PdfCompliance.Pdf17;
+
+            // Save the resultant PDF file using the above mentioned options
+            sampleDocx.Save(rutaPdf, options);
+
+            //borramos el arhivo de word temporal 
+
+            System.IO.File.Delete(rutaTemporal);
+
+
+            return RedirectToAction(nameof(AbrirArchivoPDF), new { rutaArchivo = rutaPdf });
+        }
+
+        public IActionResult AbrirArchivoPDF(string rutaArchivo)
+        {
+            // Obtener el nombre del archivo sin la ruta
+            string nombreArchivo = Path.GetFileName(rutaArchivo);
+
+            // Leer el archivo en bytes
+            byte[] contenidoArchivo = System.IO.File.ReadAllBytes(rutaArchivo);
+
+            // Configurar el encabezado de respuesta
+            Response.Headers.Add("Content-Disposition", $"inline; filename={nombreArchivo}");
+            Response.Headers.Add("Content-Type", "application/pdf");
+
+            // Devolver el contenido del archivo como un archivo PDF
+            return File(contenidoArchivo, "application/pdf");
+        }
+
+
+
+
     }
 }
 
---
+++ ContratosToyyoda/Data/ViewModels/PlantillaViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ContratosToyyoda.Data.ViewModels
+{
+    public class PlantillaViewModel
+    {
+
+        public string FilePath { get; set; }
+
+        public PlantillaViewModel(string filePath)
+        {
+            FilePath = filePath;
+        }
+    }
+}
