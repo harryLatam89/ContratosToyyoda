2dd9d917d2e52b76d748dd6d6faff065f4010cb9
harry.algoritmico1@gmail.com <harry.algoritmico1@gmail.com>
2023-05-27 10:55:40 -0600

    error message en login

--- ContratosToyyoda/Data/ViewModels/VMLogin.cs
+++ ContratosToyyoda/Data/ViewModels/VMLogin.cs
@@ -5,8 +5,10 @@ namespace ContratosToyyoda.Data.ViewModels
     public class VMLogin
     {
         [Display(Name = "EMAIL")]
+        [Required(ErrorMessage = "Un correo es obligatorio")]
         public string Email { get; set; }
         [Display(Name = "CONTRASEÑA")]
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
         public string PassWord { get; set; }
         [Display(Name = "MANTENER LA SESION INICIADA")]
         public bool KeepLoggedIn { get; set; }
